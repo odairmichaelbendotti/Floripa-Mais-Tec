96ef6f717db6a6eac38a568b5715ed137a821bcf
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-02-26 11:52:44 -0300

    Finalização do Exercício 07

--- M1S06/Exercicio-07/script.js
+++ M1S06/Exercicio-07/script.js
@@ -4,3 +4,9 @@ function changeBackgroundColor() {
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     userProfile.style.backgroundColor = randomColor;
 }
+
+function welcomeMessage() {
+    alert("Bem-vindo(a) ao nosso site!");
+}
+
+welcomeMessage()
