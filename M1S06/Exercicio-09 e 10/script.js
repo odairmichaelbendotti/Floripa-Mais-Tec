57d68784a2569b3433641cad8ec9c022ba8373cb
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-02-26 13:14:52 -0300

    Adição dos Execícios 09 e 10

--- M1S06/Exercicio-09 e 10/script.js
+++ M1S06/Exercicio-09 e 10/script.js
@@ -1,6 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    welcomeMessage();
+});
+
 function changeBackgroundColor() {
     const userProfile = document.getElementById('user-profile');
-    // Gera uma cor aleatória em hexadecimal
     const randomColor = '#' + Math.floor(Math.random()*16777215).toString(16);
     userProfile.style.backgroundColor = randomColor;
 }
@@ -10,6 +13,9 @@ function welcomeMessage() {
 }
 
 function updateUserName() {
-    var userNameElement = document.getElementById("user-name");
-    userNameElement.innerHTML = "Nome do Usuário Atualizado";   
+    var userName = prompt("Por favor, insira seu novo nome de usuário:");
+    if (userName) {
+        var userNameElement = document.getElementById("user-name");
+        userNameElement.innerHTML = `<h2>${userName}</h2>`;
+    }
 }
