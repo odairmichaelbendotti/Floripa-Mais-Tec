efe4f5382a1b3b7c9f5fa09637a189daeb1a73d7
Odair Michael Bendotti <odairmichaelbendotti@gmail.com>
2024-04-01 16:06:09 -0300

    Exercício finalizado

--- M1S10/Exercicio-01/src/components/CardTrilha.jsx
+++ M1S10/Exercicio-01/src/components/CardTrilha.jsx
@@ -1,15 +1,42 @@
-export const CardTrilha = () => {
+import PropTypes from 'prop-types';
 
-  const name = 'Odair Michael Bendotti';
+export const CardTrilha = ({nomeTrilha, cidade, estado, duracao, trajeto, dificuldade, autorTrillha, linkImagem}) => {
 
   return (
     <div className="container-trilha">
         <div className="container-img">
-
+            <img src={linkImagem} alt="imagm da trilha" />
         </div>
         <div className="container-text">
-
+            <div className='trilha-name-box'>
+                <h3>{nomeTrilha} / {cidade}-{estado}</h3>
+                <i class="fa-regular fa-heart"></i>
+            </div>
+            <div className='risco'></div>
+            <div className='autor'> 
+                <p>Por: {autorTrillha}</p>
+            </div>
+            <div className='about-trilha'>
+                <p>Duração: {duracao}</p>
+                <p>Trajeto: {trajeto}</p>
+            </div>
+            <div className='dificuldade'>
+                {dificuldade}
+            </div>
+            <div className='avaliacoes'>
+                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart"></i>
+                <i class="fa-solid fa-heart"></i>
+                <i class="fa-regular fa-heart"></i>
+            </div>
         </div>
+
     </div>
   )
 }
+
+CardTrilha.propTypes = {
+    nomeTrilha: PropTypes.string,
+    cidade: PropTypes.string
+  };
--- M1S10/Exercicio-01/src/main.jsx
+++ M1S10/Exercicio-01/src/main.jsx
@@ -7,7 +7,8 @@ import {CardTrilha} from './components/CardTrilha'
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
 
-  <CardTrilha />
+  <CardTrilha nomeTrilha='Piscinas Naturais - Barra da Lagoa' cidade='Florianópolis' estado='SC' duracao='120mins' trajeto='4km' dificuldade='Iniciante' autorTrillha='Odair Michael Bendotti' linkImagem='https://arrumaessamala.com.br/wp-content/uploads/2021/01/piscinas-naturais-barra-da-lagoa.jpg'/>
 
   </React.StrictMode>,
 )
+
